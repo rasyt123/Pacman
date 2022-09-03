6c806a591304f3db151499ddb5646f42abae8687
JustinY <rasyt123@gmail.com>
2022-09-03 13:18:24 -0700

    Update Game.cpp

--- Pacman/Game.cpp
+++ Pacman/Game.cpp
@@ -1,4 +1,6 @@
-
+//
+// Created by rasyt on 8/27/2022.
+//
 
 #include "Game.h"
 void Pacman::Game::PacGameLoop() {
@@ -8,12 +10,11 @@ void Pacman::Game::PacGameLoop() {
     sf::RenderWindow pacwindow(sf::VideoMode(50 * pacmanmap[0].size(), 50 * pacmanmap.size()), "Pacman", sf::Style::Close | sf::Style::Titlebar);
     sf::Time TimePerFrame;
     TimePerFrame = sf::seconds(1.f / 60.f);
-    Pacman pacman1(1, 2);
     pacwindow.setFramerateLimit(60);
     DrawMap(&pacwindow);
+    Pacman pacman1(1, 2, rectanglecoords);
     pacman1.RenderPacman(&pacwindow);
     pacwindow.draw(pacman1.thepacman);
-    pacwindow.display();
     while (pacwindow.isOpen())
     {
         timeSinceLastUpdate += clock.restart();
@@ -29,33 +30,6 @@ void Pacman::Game::PacGameLoop() {
     }
 }
 
-
-bool Pacman::Game::CollisonCheck(Pacman& paccolide) {
-    bool topleftcolli = false;
-    bool toprightcolli = false;
-    bool bottomleftcolli = false;
-    bool bottomrightcolli = false;
-    for (std::pair<float, float>& wall : rectanglecoords)
-    {
-        float wallxleft = wall.first;
-        float wallxright = wall.first + 50;
-        float wallytop = wall.second;
-        float wallybottom = wall.second + 50;
-        for (std::vector<float>& rectangle : paccolide.all4rects)
-        {
-            //check for an intersection in both planes
-            //both  projections must be intersecting for there to be a collision
-            if (((wallybottom >= rectangle[0] and wallybottom <= rectangle[1]) or
-            (wallytop <= rectangle[1] and wallytop >= rectangle[0]))
-             and ((rectangle[2] <= wallxright and rectangle[2] >= wallxleft)
-             or (wallxleft <= rectangle[3] and wallxleft >= rectangle[2])))
-            {
-                return true;
-            }
-        }
-    }
-    return false;
-}
 
 void Pacman::Game::RenderWall(int row, int col, sf::RenderWindow* windowptr)
 {
@@ -80,7 +54,6 @@ void Pacman::Game::RenderPellet(int row, int col, sf::RenderWindow* windowptr) {
     pellet.setPosition(floatposx + 25, floatposy + 25);
     windowptr->draw(pellet);
     std::pair<float, float> currentcoords;
-    rectanglecoords.push_back(currentcoords);
 }
 
 
@@ -95,7 +68,6 @@ void Pacman::Game::RenderPowerPellet(int row, int col, sf::RenderWindow* windowp
     bigpellet.setPosition(floatposx + 25, floatposy + 25);
     windowptr->draw(bigpellet);
     std::pair<float, float> currentcoords;
-    rectanglecoords.push_back(currentcoords);
 }
 
 
