5e0765e2ba2513a7036054c1e0284677bd1eb059
JustinY <rasyt123@gmail.com>
2022-09-07 20:02:55 -0700

    Update Animation.cpp

--- Pacman/Animation.cpp
+++ Pacman/Animation.cpp
@@ -4,32 +4,32 @@
 
 #include "Animation.h"
 
-Animation::Animation(sf::Texture *texture, sf::Vector2u imageCount, float switchTime) {
+Animation::Animation(sf::Texture *texture, sf::Vector2u imageCount, float switchTime, int col) {
     this->imageCount = imageCount;
     this->switchTime = switchTime;
     totalTime = 0.0f;
-    currentImage.x = 0;
+    currentImage.x = col;
 
     uvRect.width = texture->getSize().x / float(imageCount.x);
     uvRect.height = texture->getSize().y / float(imageCount.y);
 }
 
 
-void Animation::Update(int row, float deltaTime)
+void Animation::Update(int row,int col, int colstop, float deltaTime)
 {
     currentImage.y = row;
-    totalTime += deltaTime;
-
+    currentImage.x = col;
+   totalTime += deltaTime;
     if (totalTime >= switchTime) {
         totalTime -= switchTime;
         currentImage.x++;
-        if (currentImage.x >= imageCount.x)
+        if (currentImage.x >= colstop)
         {
-            currentImage.x = 0;
+            currentImage.x = col;
         }
     }
+
     uvRect.left = currentImage.x * uvRect.width;
     uvRect.top = currentImage.y * uvRect.height;
-
-
+    
 }
